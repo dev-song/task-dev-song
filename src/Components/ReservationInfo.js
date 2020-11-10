a592c2a933e7a65d9e89181b3888f9e9ad501111
dev-song <52951039+dev-song@users.noreply.github.com>
2020-11-10 17:30:04 +0900

    Create elements for ReservationInfo component

--- src/Components/ReservationInfo.js
+++ src/Components/ReservationInfo.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 function ReservationInfo() {
   return (
-    <div>ReservationInfo!</div>
+    <div className='ReservationInfo'>
+      <h2 className='title'>
+        기타 예약 정보
+      </h2>
+      <div className='reservation-info--container'>
+        <label className='reservation-info__label'>
+          오시는 교통 수단을 적어주세요.
+        </label>
+        <textarea className='reservation-info__input'
+          placeholder='답변을 입력해 주세요.'
+        />
+      </div>
+    </div>
   );
 }
 
