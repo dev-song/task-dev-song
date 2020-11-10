310a760e70f01f1396da025bddb5e0b8c3e5ad06
dev-song <52951039+dev-song@users.noreply.github.com>
2020-11-10 22:49:30 +0900

    Modify Gender selection operation and validate it

--- src/Components/GenderSelection.js
+++ src/Components/GenderSelection.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function GenderSelection() {
+  const [gender, setGender] = useState('');
+
+  const handleClick = e => {
+    if (gender === e.target.parentNode.value) {
+      e.target.parentNode.value = null;
+    }
+
+    setGender(e.target.parentNode.value);
+  };
+
+  const validateGender = userGender => {
+    return !!userGender;
+  }
+
   return (
     <div className='GenderSelection'>
       <label className='title'>
@@ -8,11 +22,26 @@ function GenderSelection() {
       </label>
       <select
         className='gender__select'
+        onClick={handleClick}
         size="2"
       >
-        <option className='gender__male'>남</option>
-        <option className='gender__female'>여</option>
+        <option
+          className='gender__male'
+        >
+          남
+        </option>
+        <option
+          className='gender__female'
+        >
+          여
+        </option>
       </select>
+      {!validateGender(gender)
+        ? (
+          <span className='gender__msg'>성별을 선택해주세요.</span>
+        )
+        : null
+      }
     </div>
   );
 }
