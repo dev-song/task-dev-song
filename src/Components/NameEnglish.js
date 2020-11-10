8103ae47e6a200e8925586dc2d2e028517185c1b
dev-song <52951039+dev-song@users.noreply.github.com>
2020-11-10 22:04:21 +0900

    Validate English first name & last name

--- src/Components/NameEnglish.js
+++ src/Components/NameEnglish.js
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+function validateName(name) {
+  const LEN = name.length;
+  let msg = '';
+
+  switch (true) {
+    case (LEN < 2):
+      msg = '최소 2자 이상 입력해주세요.';
+      break;
+    case (LEN > 20):
+      msg = '최대 20자까지 입력 가능합니다.';
+      break;
+    case (LEN !== name.match(/[a-zA-Z ]/g).length):
+      msg = '영어와 띄어쓰기만 입력 가능합니다.';
+      break;
+    default:
+    // console.log('유효한 이름입니다.');
+  }
+
+  return msg;
+}
 
 function NameEnglish() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const FIRST_NAME_VALIDATE_MSG = validateName(firstName);
+  const LAST_NAME_VALIDATE_MSG = validateName(lastName);
+
   return (
     <div className='NameEnglish'>
       <div className='first-name--container'>
@@ -11,7 +37,15 @@ function NameEnglish() {
           className='first-name__input'
           type='text'
           placeholder='Gil Dong'
+          value={firstName}
+          onChange={e => setFirstName(e.target.value)}
         />
+        {!!FIRST_NAME_VALIDATE_MSG
+          ? (
+            <span className='first-name__msg'>{FIRST_NAME_VALIDATE_MSG}</span>
+          )
+          : null
+        }
       </div>
       <div className='last-name--container'>
         <label className='last-name__label'>
@@ -21,7 +55,15 @@ function NameEnglish() {
           className='last-name__input'
           type='text'
           placeholder='Hong'
+          value={lastName}
+          onChange={e => setLastName(e.target.value)}
         />
+        {!!LAST_NAME_VALIDATE_MSG
+          ? (
+            <span className='last-name__msg'>{LAST_NAME_VALIDATE_MSG}</span>
+          )
+          : null
+        }
       </div>
     </div>
   );
