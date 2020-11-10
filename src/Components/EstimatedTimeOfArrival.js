ef600bdcc7942bffa404782dcedbde101bc9dc91
dev-song <52951039+dev-song@users.noreply.github.com>
2020-11-11 03:03:05 +0900

    Validate ETA in EstimatedTimeOfArrival
    Array creation functions are also refactored

--- src/Components/EstimatedTimeOfArrival.js
+++ src/Components/EstimatedTimeOfArrival.js
@@ -1,27 +1,15 @@
-import React from 'react';
-
-function createHourList() {
-  const HOURS = [];
-  for (let hour = 0; hour < 24; hour++) {
-    HOURS.push(hour);
-  }
-
-  return HOURS;
-}
-
-function createMinuteList() {
-  const MINUTES = [];
-  const INTERVAL = 10;
-  for (let minute = 0; minute < 60; minute += INTERVAL) {
-    MINUTES.push(minute);
-  }
-
-  return MINUTES;
-}
+import React, { useState } from 'react';
 
 function EstimatedTimeOfArrival() {
-  const hours = createHourList();
-  const minutes = createMinuteList();
+  const createHourList = () => new Array(24).fill(0).map((v, i) => i);
+  const createMinuteList = () => new Array(60).fill(0).map((v, i) => i);
+  const validateTime = (hourText, minuteText) => {
+    if (hourText === '시' || minuteText === '분') return false;
+    return true;
+  }
+
+  const [hourSelected, setHourSelected] = useState('시');
+  const [minuteSelected, setMinuteSelected] = useState('분');
 
   return (
     <div className='EstimatedTimeOfArrival'>
@@ -29,18 +17,30 @@ function EstimatedTimeOfArrival() {
         숙소 도착 예정 시간
       </h2>
       <div className='eta__form'>
-        <select className='eta__hour'>
+        <select
+          className='eta__hour'
+          onChange={e => setHourSelected(e.target.value)}
+        >
           <option defaultValue>시</option>
-          {hours.map(hour => (
+          {createHourList().map(hour => (
             <option key={hour}>{hour}시</option>
           ))}
         </select>
-        <select className='eta__minute'>
+        <select
+          className='eta__minute'
+          onChange={e => setMinuteSelected(e.target.value)}
+        >
           <option defaultValue>분</option>
-          {minutes.map(minute => (
+          {createMinuteList().map(minute => (
             <option key={minute}>{minute}분</option>
           ))}
         </select>
+        {!validateTime(hourSelected, minuteSelected)
+          ? (
+            <span className='eta__msg'>숙소 도착 예정 시간을 선택해주세요.</span>
+          )
+          : null
+        }
       </div>
     </div>
   );
