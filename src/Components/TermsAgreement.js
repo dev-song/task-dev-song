e8beaf4740cd6c7da3192e81f0e7a18e285f48d4
dev-song <52951039+dev-song@users.noreply.github.com>
2020-11-11 18:44:59 +0900

    Make state hooks for TermsAgreement and save checked properties in them

--- src/Components/TermsAgreement.js
+++ src/Components/TermsAgreement.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function TermsAgreement() {
+  const [wholeAgreement, setWholeAgreement] = useState(false);
+  const [mainAgreement, setMainAgreement] = useState(false);
+  const [subAgreement, setSubAgreement] = useState(false);
+
+  const handleAllTerms = e => {
+    const CHECKED = e.target.checked;
+
+    setWholeAgreement(CHECKED);
+    setMainAgreement(CHECKED);
+    setSubAgreement(CHECKED);
+  }
+
+  useEffect(() => {
+    setWholeAgreement(mainAgreement && subAgreement);
+  }, [mainAgreement, subAgreement]);
+
   return (
     <div className='TermsAgreement'>
       <h2 className='title'>
@@ -10,6 +26,8 @@ function TermsAgreement() {
         <input
           className='agreement-full__checkbox'
           type='checkbox'
+          checked={wholeAgreement}
+          onChange={e => handleAllTerms(e)}
         />
         <label className='agreement-full__label'>
           전체 약관 동의
@@ -19,19 +37,23 @@ function TermsAgreement() {
             <input
               className='sub-terms__checkbox'
               type='checkbox'
+              checked={mainAgreement}
+              onChange={() => setMainAgreement(!mainAgreement)}
             />
             <label className='sub-terms__label'>
               여행자 약관 동의 (필수)
-              </label>
+            </label>
           </div>
           <div className='sub-terms--container'>
             <input
               className='sub-terms__checkbox'
               type='checkbox'
+              checked={subAgreement}
+              onChange={() => setSubAgreement(!subAgreement)}
             />
             <label className='sub-terms__label'>
               특가 항공권 및 할인 혜택 안내 동의 (선택)
-              </label>
+            </label>
           </div>
         </div>
       </div>
