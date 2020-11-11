483b4e42800dda6fd78ab9143707ce05b6138346
dev-song <52951039+dev-song@users.noreply.github.com>
2020-11-11 19:12:50 +0900

    Transfer mainTermAgreed state of the App to Payment to activate the button

--- src/Components/Payment.js
+++ src/Components/Payment.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
-function Payment() {
+function Payment({ mainTermAgreed }) {
+  const CLASS_WITHOUT_AGREEMENT = 'payment__submit';
+  const CLASS_WITH_AGREEMENT = `${CLASS_WITHOUT_AGREEMENT} active`;
+
   return (
     <div className='Payment'>
       <input
-        className='payment__submit'
+        className={
+          mainTermAgreed
+            ? CLASS_WITH_AGREEMENT
+            : CLASS_WITHOUT_AGREEMENT
+        }
         type='submit'
         value='결제하기'
       />
