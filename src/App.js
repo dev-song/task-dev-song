bb149c763fee5efc6a14c490b042d4f054a229f8
dev-song <52951039+dev-song@users.noreply.github.com>
2020-11-11 23:43:25 +0900

    Display overlay notifying the completion of the reservation on payment button click

--- src/App.js
+++ src/App.js
@@ -19,37 +19,45 @@ function App() {
   }
 
   return (
-    <form
-      className="App"
-      onSubmit={e => {
-        e.preventDefault();
-        setPaymentRequested(true);
-      }}
-    >
-      <TravelerInfo
-        paymentRequested={paymentRequested}
-        passValidation={passValidation}
-      />
-      <EstimatedTimeOfArrival
-        paymentRequested={paymentRequested}
-        passValidation={passValidation}
-      />
-      <ContactInfo
-        paymentRequested={paymentRequested}
-        passValidation={passValidation}
-      />
-      <ReservationInfo
-        paymentRequested={paymentRequested}
-        passValidation={passValidation}
-      />
-      <TermsAgreement
-        handleMainTerm={handleMainTerm}
-      />
-      <Payment
-        mainTermAgreed={mainTermAgreed}
-        validationPassed={validationPassed}
-      />
-    </form>
+    <div className="App">
+      {paymentRequested && validationPassed
+        ? <div className="Overlay">예약이 완료되었습니다.</div>
+        : null
+      }
+      <form
+        className="Reservation"
+        onSubmit={e => {
+          e.preventDefault();
+
+          if (!mainTermAgreed) return;
+          setPaymentRequested(true);
+        }}
+      >
+        <TravelerInfo
+          paymentRequested={paymentRequested}
+          passValidation={passValidation}
+        />
+        <EstimatedTimeOfArrival
+          paymentRequested={paymentRequested}
+          passValidation={passValidation}
+        />
+        <ContactInfo
+          paymentRequested={paymentRequested}
+          passValidation={passValidation}
+        />
+        <ReservationInfo
+          paymentRequested={paymentRequested}
+          passValidation={passValidation}
+        />
+        <TermsAgreement
+          handleMainTerm={handleMainTerm}
+        />
+        <Payment
+          mainTermAgreed={mainTermAgreed}
+          validationPassed={validationPassed}
+        />
+      </form>
+    </div>
   );
 }
 
