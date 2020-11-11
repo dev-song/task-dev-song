0c3393f4535401d539f02003afc740d2118ec1a3
dev-song <52951039+dev-song@users.noreply.github.com>
2020-11-11 23:08:59 +0900

    Handle paymentRequested state on form submit, and transfer it to sub-components

--- src/App.js
+++ src/App.js
@@ -9,16 +9,31 @@ import Payment from './Components/Payment';
 
 function App() {
   const [mainTermAgreed, setMainTermAgreed] = useState(false);
+  const [paymentRequested, setPaymentRequested] = useState(false);
   const handleMainTerm = agreed => {
     setMainTermAgreed(agreed);
   };
 
   return (
-    <form className="App">
-      <TravelerInfo />
-      <EstimatedTimeOfArrival />
-      <ContactInfo />
-      <ReservationInfo />
+    <form
+      className="App"
+      onSubmit={e => {
+        e.preventDefault();
+        setPaymentRequested(true);
+      }}
+    >
+      <TravelerInfo
+        paymentRequested={paymentRequested}
+      />
+      <EstimatedTimeOfArrival
+        paymentRequested={paymentRequested}
+      />
+      <ContactInfo
+        paymentRequested={paymentRequested}
+      />
+      <ReservationInfo
+        paymentRequested={paymentRequested}
+      />
       <TermsAgreement
         handleMainTerm={handleMainTerm}
       />
